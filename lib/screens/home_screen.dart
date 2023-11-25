5bba23bf4370a09f359d30b6c1ac71d897e3c8b6
irfanarfianto <irfanarfianto92@gmail.com>
2023-11-25 22:21:36 +0700

    Update package name and user name

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,90 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class HomeScreen extends StatelessWidget {
-  final String userName = "John Doe"; // Ganti dengan nama pengguna yang sesuai
+  final String userName =
+      "Irfan Arfianto"; // Ganti dengan nama pengguna yang sesuai
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'E-Learning App',
-          style: TextStyle(
-            color: Color(0xFFE4F1F8),
-            fontSize: 17.28,
-            fontFamily: 'DM Sans',
-            fontWeight: FontWeight.w500,
-            height: 0,
-          ),
-        ),
-        backgroundColor: Color(0xFF0C042E),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Header Section with User Name and Profile Photo
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                color: Color(0xFF543EE9),
-                borderRadius: BorderRadius.circular(17.0),
-              ),
-              child: Row(
-                children: [
-                  CircleAvatar(
-                    radius: 24,
-                    // Ganti dengan foto profil pengguna
-                    backgroundImage: AssetImage('assets/profile.jpg'),
-                  ),
-                  SizedBox(width: 16),
-                  Text(
-                    'Hello, $userName!',
-                    style: TextStyle(
-                      color: Color(0xFFE4F1F8),
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
+        // appBar: AppBar(
+        //   title: Text(
+        //     'E-Learning App',
+        //     style: TextStyle(
+        //       color: Color(0xFFE4F1F8),
+        //       fontSize: 17.0,
+        //       fontFamily: 'DM Sans',
+        //       fontWeight: FontWeight.w500,
+        //     ),
+        //   ),
+        //   backgroundColor: Color(0xFF0C042E),
+        // ),
+        body: Container(
+          decoration: BoxDecoration(color: Color(0xFF0C042E)),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Header Section with User Name and Profile Photo
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Color(0xFF543EE9),
+                  borderRadius: BorderRadius.circular(17.0),
+                ),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 24,
+                      // Ganti dengan foto profil pengguna
+                      backgroundImage: AssetImage('assets/profile.JPG'),
                     ),
-                  ),
-                ],
+                    SizedBox(width: 16),
+                    Text(
+                      'Hello, $userName!',
+                      style: TextStyle(
+                        color: Color(0xFFE4F1F8),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            // Carousel Section
-            Container(
-              height: 200, // Sesuaikan tinggi carousel sesuai kebutuhan
-              child: Placeholder(), // Ganti dengan widget Carousel yang sesuai
-            ),
-            // Tambahkan bagian lainnya sesuai kebutuhan
-          ],
+              SizedBox(height: 16),
+              // Carousel Section
+              Container(
+                height: 200, // Sesuaikan tinggi carousel sesuai kebutuhan
+                color: Color(
+                    0xFF543EE9), // Ganti dengan warna latar belakang yang sesuai
+                child: Center(
+                  child: Text(
+                    'Ganti dengan widget Carousel',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+              // Tambahkan bagian lainnya sesuai kebutuhan
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        selectedItemColor: Colors.blue,
-        currentIndex:
-            0, // Ganti sesuai dengan indeks halaman yang ingin ditampilkan
-        onTap: (int index) {
-          // Handle navigasi ketika item bottom navigation bar ditekan
-          // Misalnya, menggunakan Navigator untuk pindah halaman
-          // Navigator.of(context).pushNamed('/halaman_ke-${index + 1}');
-        },
-      ),
-    );
+        bottomNavigationBar: ConvexAppBar(
+          style: TabStyle.fixedCircle,
+          height: 60,
+          curveSize: 100,
+          backgroundColor: Color(0xFF0C042E),
+          color: Color.fromARGB(255, 31, 31, 32),
+          items: [
+            TabItem(icon: Icons.home, title: 'Home'),
+            TabItem(icon: Icons.book, title: 'Course'),
+            TabItem(icon: Icons.search, title: 'Search'),
+            TabItem(icon: Icons.message, title: 'Message'),
+            TabItem(icon: Icons.people, title: 'Profile'),
+          ],
+          onTap: (int i) => print('click index=$i'),
+        ));
   }
 }
 
